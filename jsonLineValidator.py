e49cc35dc019ba1d2812f758b2631c0b351e4cf9
aristila <57129740+aristila@users.noreply.github.com>
2020-02-10 13:00:50 +0200

    Update jsonLineValidator.py

--- jsonLineValidator.py
+++ jsonLineValidator.py
@@ -11,13 +11,13 @@ badLines = list()
 # iterate through files
 print('Iterating through ', len(filenames), ' files:')
 for file in filenames:
+    print('Reading file', file)
     with codecs.open(file, 'r', encoding='utf-8', errors='replace') as f:
         linenum = 0
         for line in f.readlines():
             linenum+=1
             try:
                 json.loads(line)
-                print(file, f'Line {linenum} ok.')
             except:
                 print(file, f'Line {linenum} CORRUPTED!')
                 badLines.append((file, linenum))
@@ -25,6 +25,6 @@ for file in filenames:
 # write a file with all the bad file info 
 with codecs.open('bad_lines.txt', 'a', encoding='utf-8', errors='replace') as sf:
     for i in badLines:
-        sf.write(i[0]+' line '+i[1])
+        sf.write(i[0]+' line 'str(+i[1]))
 
 print('Done!')

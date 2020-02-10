471dca2c93a7a58f0708931c7151ccf6f017f28d
aristila <57129740+aristila@users.noreply.github.com>
2020-02-10 12:44:51 +0200

    Add files via upload

---
+++ jsonLineValidator.py
@@ -0,0 +1,30 @@
+# jsonlines validator with utf-8
+
+import json, os, codecs
+
+# get a list of jsonfiles in the directory this code is in
+filenames = [file for file in os.listdir() if file.endswith('.json')]
+
+# collect bad line info here
+badLines = list()
+
+# iterate through files
+print('Iterating through ', len(filenames), ' files:')
+for file in filenames:
+    with codecs.open(file, 'r', encoding='utf-8', errors='replace') as f:
+        linenum = 0
+        for line in f.readlines():
+            linenum+=1
+            try:
+                json.loads(line)
+                print(file, f'Line {linenum} ok.')
+            except:
+                print(file, f'Line {linenum} CORRUPTED!')
+                badLines.append((file, linenum))
+
+# write a file with all the bad file info 
+with codecs.open('bad_lines.txt', 'a', encoding='utf-8', errors='replace') as sf:
+    for i in badLines:
+        sf.write(i[0]+' line '+i[1])
+
+print('Done!')
